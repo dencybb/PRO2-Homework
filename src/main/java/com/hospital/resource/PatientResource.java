677334aff76fe06bbbb5898619da8445e71183bc
Denis Basic <denis.dency1999@yahoo.com>
2026-06-30 16:42:58 +0200

    Peti domaci zadatak

--- src/main/java/com/hospital/resource/PatientResource.java
+++ src/main/java/com/hospital/resource/PatientResource.java
@@ -1,11 +1,14 @@
 package com.hospital.resource;
 
+import com.hospital.dto.FileUploadForm;
 import com.hospital.entity.Patient;
 import com.hospital.service.PatientService;
 import com.hospital.service.TimeZoneService;
+import com.hospital.service.UploadedFileService;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
 import java.util.List;
 
@@ -19,6 +22,9 @@ public class PatientResource {
 
     @Inject
     TimeZoneService timeZoneService;
+
+    @Inject
+    UploadedFileService uploadedFileService;
 
     @POST
     public Patient save(Patient patient) {
@@ -46,5 +52,18 @@ public class PatientResource {
     @Path("/getTimezoneByIP")
     public Patient getTimezoneByIP(@QueryParam("userId") Long userId) {
         return timeZoneService.assignTimeZone(userId);
+    }
+
+    @POST
+    @Path("/uploadFile")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Patient uploadFile(@QueryParam("id") Long id, @MultipartForm FileUploadForm form) {
+        return uploadedFileService.uploadFile(id, form);
+    }
+
+    @GET
+    @Path("/{id}/files")
+    public Patient getPatientWithFiles(@PathParam("id") Long id) {
+        return uploadedFileService.getPatientWithFiles(id);
     }
 }
